b1dc1e061367dc738d8af9e7425da1cee9c87663
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-08 18:24:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,6 +8,7 @@ def directors_totals(nds)
   # that you know what you're starting with!
   #
   #
+  
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
   }
